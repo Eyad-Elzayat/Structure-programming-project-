6931f55bd93c8880ab2ad5cd8a35459983efe9b0
Dod <elzayateyad@gmail.com>
2025-12-12 17:07:07 +0200

    setting up main bulding

--- Banking system/src/Main.java
+++ Banking system/src/Main.java
@@ -1,5 +1,57 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+    //methods
+    public static void accountCreation(){
+
+    }
+    public static void depositMoney(){
+
+    }
+    public static void withdrawMoney(){
+
+    }
+    public static void transferFunds(){
+
+    }
+    public static void interestCal(){
+
+    }
+
+    public static void main(String[] args) {
+        //setting up scanner
+        Scanner sc = new Scanner(System.in);
+
+        //setting up main data structure
+        int maxUsers = 50;
+        int[] accountNumbers = new int[maxUsers];
+        String[] accountNames = new String[maxUsers];
+        double[] balances = new double[maxUsers];
+        int[] accountTypes = new int[maxUsers];
+        int[] withdrawalsCount = new int[maxUsers];
+
+        //main functions
+        System.out.println("Please select the operation you want: ");
+        System.out.println("1. Account Creation");
+        System.out.println("2. Deposit Money");
+        System.out.println("3. Withdraw Money");
+        System.out.println("4. Transfer Funds");
+        System.out.println("5. Interest Calculator");
+
+        int operation  = sc.nextInt();
+
+        if (operation == 1) {
+            accountCreation();
+        }else if (operation == 2) {
+            depositMoney();
+        }else if (operation == 3) {
+            withdrawMoney();
+        }else if (operation == 4) {
+            transferFunds();
+        }else if (operation == 5) {
+            interestCal();
+        }else {
+            System.out.println("This operation is not found, please try again");
+        }
     }
 }
