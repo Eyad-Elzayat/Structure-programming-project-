b3d738c65e18f44158181be2a781593629d64979
mariyam <mkholafa309@gmail.com>
2025-12-21 17:53:17 +0200

    add comments to the code

--- Banking system/src/Main.java
+++ Banking system/src/Main.java
@@ -5,36 +5,36 @@ import java.util.Scanner;
 public class Main {
     //methods
     public static void accountCreation() {
-        int[] account = new int[50];
-        int index=0;
-        int accountnumber = 1000;
-        while (true) {
-            Scanner input = new Scanner(System.in);
-            System.out.println("Choose your account type\n1-Checking\n2-Saving\nEnter account type:");
-            int accounttype = input.nextInt();
-            if (accounttype == 2) {
-                System.out.println("Enter amount:");
-                double amount = input.nextDouble();
-                if (amount >= 100) {
-                    accountnumber++;
-                    account[index] = accounttype;
-                    index++;
-                    System.out.println("Saving account created successfully");
-                    System.out.println("Accountnumber:" + accountnumber);
-                    System.out.println("Accounttype:" + "saving");
-                    System.out.println("Balance:" + amount);
-                } else {
-                    System.out.println("Cannot create saving account\nMinimum amount is 100");
+        int[] account = new int[50];//array of size 50
+        int index=0;//start point of array
+        int accountnumber = 1000;//last account number
+        while (true) {//loop so the program keeps running continuously
+            Scanner input = new Scanner(System.in);//open scanner
+            System.out.println("Choose your account type\n1-Checking\n2-Saving\nEnter account type:");//ask the user to enter account type
+            int accounttype = input.nextInt();//read an integer number from the user
+            if (accounttype == 2) {//if the user chose saving account
+                System.out.println("Enter amount:");//ask the user to enter amount
+                double amount = input.nextDouble();//read a decimal number from user
+                if (amount >= 100) {//if the amount the user entered is more than 100 account will be created successfully
+                    accountnumber++;//generate sequential number
+                    account[index] = accounttype;//store the account number in the first index
+                    index++;//move to the next index
+                    System.out.println("Saving account created successfully");//display message when a saving account is successfully created
+                    System.out.println("Accountnumber:" + accountnumber);//display account number to the user
+                    System.out.println("Accounttype:" + "saving");//display account type to the user
+                    System.out.println("Balance:" + amount);//display account balance to the user
+                } else {// if the amount less than 100 account will not be created
+                    System.out.println("Cannot create saving account\nMinimum amount is 100");//display message if minimum deposit is less than 100
                 }
-            } else if (accounttype == 1) {
-                accountnumber++;
-                account[index] = accounttype;
-                index++;
-                System.out.println("Checking account created successfully");
-                System.out.println("Account number=" + accountnumber);
-                System.out.println("Accounttype:" + "checking");
-            } else {
-                System.out.println("invalid account type");
+            } else if (accounttype == 1) {// if the user chose checking account
+                accountnumber++;//generate sequential number
+                account[index] = accounttype;//store the account number
+                index++;//move to the next index
+                System.out.println("Checking account created successfully");//display message when checking account is created
+                System.out.println("Account number=" + accountnumber);//display account number to the user
+                System.out.println("Accounttype:" + "checking");//display account type to the user
+            } else {//if the user enters any number other than 1 and 2 it will be rejected
+                System.out.println("invalid account type");//display message if any number other than 1 and 2 is chosen
             }
         }
     }
